7e642bce260deb5b72063bb68d453991234e4f32
Pernilla Sterner <psterner@hotmail.com>
2024-05-02 09:00:28 +0200

    Add comments

--- 11-fs-async.js
+++ 11-fs-async.js
@@ -1,24 +1,26 @@
 import { readFile, writeFile } from "fs";
 
-// When funcionality is complete, then we run the callback
+/// Read the content from the first file
 readFile("./content/first.txt", "utf8", (err, result) => {
   if (err) {
     console.log(err);
     return;
   }
-  const first = result;
+  const first = result; // Save the result from the first file
 
+  // Read the content from the second file
   readFile("./content/second.txt", "utf8", (err, result) => {
     if (err) {
       console.log(err);
       return;
     }
 
-    const second = result;
+    const second = result; // Save the result from the second file
 
+    // Write the combined result to a new file
     writeFile(
-      "./content/result-async.txt",
-      `Here is the result: ${first}, ${second}`,
+      "./content/result-async.txt", // Path to the new file
+      `Here is the result: ${first}, ${second}`, // Path to the new file
       (err, result) => {
         if (err) {
           console.log(err);
