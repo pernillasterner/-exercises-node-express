d6481cc7e981bac6f80b926f7685e55b7e1836c0
Pernilla Sterner <psterner@hotmail.com>
2024-05-02 09:03:59 +0200

    Logg process of fs sync

--- 10-fs-sync.js
+++ 10-fs-sync.js
@@ -1,5 +1,5 @@
 import { readFileSync, writeFileSync } from "fs";
-
+console.log("start");
 const first = readFileSync("./content/first.txt", "utf8");
 const second = readFileSync("./content/second.txt", "utf8");
 
@@ -11,3 +11,6 @@ writeFileSync(
   `Here is the result : ${first}, ${second}`,
   { flag: "a" }
 );
+
+console.log("done with this task");
+console.log("staring the text task");
